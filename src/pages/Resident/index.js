b1bfcbf38916559c2241c82474f3ec711b5a21db
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-05-23 15:34:02 -0300

    Datepicker

--- src/pages/Resident/index.js
+++ src/pages/Resident/index.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link, withRouter, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import DatePicker from 'react-datepicker';
 
+import api from '../../services/api';
 import firebase from '../../firebase/config';
 
 import './style.css';
+import 'react-datepicker/dist/react-datepicker.css'
 
 import logoImg from '../../assets/logo.svg';
 
@@ -29,10 +32,18 @@ const NewResident = () => {
         })
     })
 
+    const resetFields = () => {
+        setName('');
+        setEmail('');
+        setCpf('');
+        setBirthDate('');
+        setPhone('');
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(userState);
-        
+
         const data = {
             name,
             email,
@@ -42,6 +53,30 @@ const NewResident = () => {
         };
 
         console.log(data);
+
+        try {
+            const token = await userState.getIdToken();
+
+            const response = await api.post('v1/residents', data, { headers: { 'Authorization': `Bearer ${token}` } });
+
+            if (!response.data.success) {
+                //alert(response.data.message);
+                let messages = "";
+                if (response.data.data.length > 0) {
+                    response.data.data.forEach((item) => {
+                        messages += item.message + "\n";
+                    })
+                    alert(messages);
+                }
+            }
+            else {
+                alert(response.data.message);
+                resetFields();
+
+            }
+        } catch (error) {
+
+        }
     }
 
     return (
@@ -62,34 +97,39 @@ const NewResident = () => {
                 <form onSubmit={handleSubmit}>
                     <input
                         placeholder="Name"
-                    value={name}
-                    onChange={e => setName(e.target.value)}
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        required
                     />
                     <input
                         type="email"
                         placeholder="E-mail"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        required
                     />
                     <div className="input-group">
                         <input
                             placeholder="CPF"
-                        value={cpf}
-                        onChange={e => setCpf(e.target.value)}
+                            value={cpf}
+                            onChange={e => setCpf(e.target.value)}
+                            required
                         />
-
-                        <input
-                            placeholder="Birth Date"
-                            style={{ width: 140 }}
-                        value={birthDate}
-                        onChange={e => setBirthDate(e.target.value)}
+                        <DatePicker
+                            selected={birthDate}
+                            onChange={date => setBirthDate(date)}
+                            dateFormat="dd/MM/yyyy"
+                            placeholderText="Birth Date"
+                            className="datepicker"
+                            required
                         />
                     </div>
-                    <input 
-                        placeholder="Phone" 
+                    <input
+                        placeholder="Phone"
                         value={phone}
                         onChange={e => setPhone(e.target.value)}
-                        />
+                        required
+                    />
                     <button className="button" type="submit">Create</button>
                 </form>
             </div>
